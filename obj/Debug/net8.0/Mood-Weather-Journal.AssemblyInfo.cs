37943e2e18ff57e9b796d6a7f97adb2b3b471b1d
Phanuelle <phanmanuel9@gmail.com>
2025-05-23 22:20:12 -0500

    deploy changes

--- obj/Debug/net8.0/Mood-Weather-Journal.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mood-Weather-Journal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mood-Weather-Journal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fef3ba11927adb06291831c9e6de1d79e3aebc04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25a621ac0548d42822e8b4f6936646b101ee9fa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mood-Weather-Journal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mood-Weather-Journal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
